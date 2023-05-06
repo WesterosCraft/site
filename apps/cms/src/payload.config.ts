a4f15b03d09852c33c0dfe945e809797d7fe6584
jacob.granberry <jacobgranberry@gmail.com>
2023-05-06 13:52:24 -0500

    initial config

---
+++ apps/cms/src/payload.config.ts
@@ -0,0 +1,16 @@
+import path from 'path'
+import { buildConfig } from 'payload/config'
+
+import { Pages } from './collections/Pages'
+import { Users } from './collections/Users'
+import { MainMenu } from './globals/MainMenu'
+
+export default buildConfig({
+  collections: [Pages, Users],
+  cors: [process.env.PAYLOAD_PUBLIC_SERVER_URL, process.env.PAYLOAD_PUBLIC_SITE_URL],
+  csrf: [process.env.PAYLOAD_PUBLIC_SERVER_URL, process.env.PAYLOAD_PUBLIC_SITE_URL],
+  globals: [MainMenu],
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+})
