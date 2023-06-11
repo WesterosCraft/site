93c8c79e01fd604962a5d06d50d8fa1aed5814a8
jacob.granberry <jacobgranberry@gmail.com>
2023-06-11 11:45:25 -0500

    mostly completes home page, completes rooekry page

---
+++ apps/web/src/components/rich-text/rich-text.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import React from 'react'
+import { CustomRenderers, Serialize } from './serialize'
+
+export const RichText: React.FC<{
+  className?: string
+  content: any
+  customRenderers?: CustomRenderers
+}> = ({ content, customRenderers }) => {
+  if (!content) {
+    return null
+  }
+
+  return (
+    <div>
+      <Serialize content={content} customRenderers={customRenderers} />
+    </div>
+  )
+}
---
+++ apps/web/src/lib/utils.ts
@@ -0,0 +1,21 @@
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function camelize(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+}
+
+export function titleize(slug: string) {
+  var words = slug.split("-");
+  return words
+    .map(function (word) {
+      return word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
+    })
+    .join(" ");
+}
