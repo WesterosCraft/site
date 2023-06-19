de9752b8cdb54ec6775f28f4db9fcce1750cb0c6
jacob.granberry <jacobgranberry@gmail.com>
2023-06-19 17:28:52 -0500

    sets up nested project details using slots

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -1,88 +1,90 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: ["class"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     container: {
       center: true,
-      padding: '2rem',
+      padding: "2rem",
       screens: {
-        '2xl': '1400px',
+        "2xl": "1400px",
       },
     },
     extend: {
       colors: {
-        primaryLight: '#FFFCF1',
-        primaryRed: '#9B2C2C',
-        primaryDark: '#22261F',
-        primaryGold: '#daaf3c',
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
+        primaryLight: "#FFFCF1",
+        primaryRed: "#9B2C2C",
+        primaryDark: "#22261F",
+        primaryGold: "#daaf3c",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
         destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
         muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
         accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
         popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
         card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        'accordion-down': {
+        "accordion-down": {
           from: { height: 0 },
-          to: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       backgroundImage: {
-        'square-pattern': 'url(/bright-squares.png)',
+        "square-pattern": "url(/bright-squares.png)",
+        "gradient-radial":
+          "radial-gradient(98.71% 98.71% at 52.79% 99.41%, #FFC5C0 0%, rgba(255, 241, 192, 0) 100%)",
       },
       maxWidth: {
-        '8xl': '90rem',
+        "8xl": "90rem",
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
-}
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
--- web copy/components/bordered-container.tsx
+++ web copy/components/bordered-container.tsx
@@ -9,14 +9,14 @@ export function ContainerBorder({
   className?: React.ComponentProps<'div'>['className']
   borderBottom?: boolean
 }) {
-  const borderBottomStyle = borderBottom ? 'border-b-[1.5px]' : ''
+  const borderBottomStyle = borderBottom ? 'border-b-2' : ''
   const borderColorStyle = variant === 'light' ? 'border-primaryDark' : 'border-primaryGold'
 
   return (
     <section className="w-full">
       <div className="max-w-7xl mx-auto px-2 md:px-5">
         <div
-          className={`flex flex-col w-full mx-auto border-l-0 sm:border-l-[1.5px] border-r-0 sm:border-r-[1.5px] ${borderColorStyle} ${className} ${borderBottomStyle}`}
+          className={`flex flex-col w-full mx-auto border-l-0 sm:border-l-2 border-r-0 sm:border-r-2 ${borderColorStyle} ${className} ${borderBottomStyle}`}
         >
           {children}
         </div>
